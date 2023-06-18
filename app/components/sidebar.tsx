d5d2d7e2b975bceb34a3009b2d98697ee1fd9049
S0225s <71736259+S0225s@users.noreply.github.com>
2023-06-18 14:43:09 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>梦辉哥哥的小灰灰AI</div>
+        <div className={styles["sidebar-title"]}>Ghost</div>
         <div className={styles["sidebar-sub-title"]}>
-         ❤❤❤
+         问答、绘画AI助手
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
